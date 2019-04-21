c38b03b646dde5a98c5055727219e1dba700114d
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2019-04-21 17:49:36 +0300

    fix font path

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -76,8 +76,7 @@ module.exports = {
                 test: /\.(ttf|eot|woff|woff2)$/,
                 loader: 'file-loader',
                 options: {
-                    name: '[name].[ext]',
-                    outputPath: '../../pisa/dist/assets/fonts/'
+                    name: '/assets/fonts/[name].[ext]'
                 }
             }]
     },
